fb9564251c0e4f9b71ff23b0576dd69c193e629d
kleidione Freitas <kleidione@gmail.com>
2024-07-09 22:21:52 -0300

    veux: parts: Drop doze settings
    Signed-off-by: kleidione <kleidione@gmail.com>
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,7 +22,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
@@ -32,9 +31,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
-        DozeUtils.onBootCompleted(context);
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);        
     }
